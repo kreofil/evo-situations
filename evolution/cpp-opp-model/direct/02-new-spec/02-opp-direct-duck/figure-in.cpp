b2ae67e2c1d5d2910673b4ac72935948e363e3b9
kreofil <kreofil@mail.ru>
2024-12-25 20:34:39 +0300

    new evolution programs

---
+++ evolution/cpp-opp-model/direct/02-new-spec/02-opp-direct-duck/figure-in.cpp
@@ -0,0 +1,40 @@
+//------------------------------------------------------------------------------
+// figure-in.cpp - содержит процедуру ввода параметров
+// для уже созданной любой фигуры
+//------------------------------------------------------------------------------
+
+// #include <iostream>
+#include "figure.h"
+
+//------------------------------------------------------------------------------
+// Необходима подключить информацию обо всех имеющихся
+// геометрических фигурах
+#include "rectangle.h"
+#include "triangle.h"
+#include "circle.h"
+
+//------------------------------------------------------------------------------
+// Ввод параметров обобщенной фигуры из стандартного потока ввода
+Figure* Figure::In(std::ifstream &ifst) {
+  Figure *sp;
+  Any *anyFigure;
+  int k;
+  ifst >> k;
+  switch(k) {
+    case 1:
+      anyFigure = new Rectangle;
+      break;
+    case 2:
+      anyFigure = new Triangle;
+      break;
+    case 3:
+      anyFigure = new Circle;
+      break;
+    default:
+      return 0;
+  }
+  sp = new Figure;
+  sp->specObject = anyFigure;
+  sp->InFP(ifst);
+  return sp;
+}
